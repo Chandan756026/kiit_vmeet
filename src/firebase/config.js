c64f2bc1b0f650d1a9b6013582247e9a223e5cdb
Chandan756026 <chandangiri30968@gmail.com>
2025-03-13 17:46:22 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   // your firebase config
-  apiKey: "AIzaSyAHmFGYkYU8lR5QoVzlAgkkeWzbounf9UQ",
-  authDomain: "meetify1-bd3bd.firebaseapp.com",
-  projectId: "meetify1-bd3bd",
-  storageBucket: "meetify1-bd3bd.appspot.com",
-  messagingSenderId: "21939810359",
-  appId: "1:21939810359:web:0fc35b136c8c73b3e68fb2",
-  measurementId: "G-L6BY6L11HF",
+ apiKey: "AIzaSyCR8KNLh5YCOcY19u6sTtkHWCao4rkeW-Q",
+  authDomain: "sonic-meet-7b826.firebaseapp.com",
+  projectId: "sonic-meet-7b826",
+  storageBucket: "sonic-meet-7b826.firebasestorage.app",
+  messagingSenderId: "554327446243",
+  appId: "1:554327446243:web:0185f6d1c913f8b147ebd6",
+  measurementId: "G-XG4Y2TMTN1"
 };
 
 const app = initializeApp(firebaseConfig);
